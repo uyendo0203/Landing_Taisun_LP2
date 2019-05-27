8fd642318d25e8abcda256a46fc30df718975435
uyendo <uyen.do@apps-cyclone.com>
2019-05-27 23:53:46 +0700

    update page done

--- src/scripts/public/index.js
+++ src/scripts/public/index.js
@@ -13,8 +13,7 @@ function changeViewDetect(){
     }
     if($(window).width() < 480){
         $('.taisun-lp2__home--left').css({
-            'transform':'scale(0.8)',
-            'margin-top':0
+            'transform':'scale(0.9)',
         })
     }
 }
@@ -29,3 +28,27 @@ $(window).on('resize',function(){
 
 $(".rule-content").mCustomScrollbar();
 $(".list-result").mCustomScrollbar();
+
+/**
+ * 
+ */
+
+TweenLite.set("flower-animate",{perspective:600})
+
+var total = 30;
+var warp = document.getElementById("flower-animate"),	w = window.innerWidth , h = window.innerHeight;
+ 
+ for (i=0; i<total; i++){ 
+   var Div = document.createElement('div');
+   TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   warp.appendChild(Div);
+   animm(Div);
+ }
+ 
+ function animm(elm){   
+   TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+   TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+   TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+ };
+
+function R(min,max) {return min+Math.random()*(max-min)};
